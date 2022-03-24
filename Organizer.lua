8dd46f34fbd3287fa28336dafb57077c37377905
Nasif <39343333+profake@users.noreply.github.com>
2022-03-24 20:40:19 +0600

    Added coloring when new messages arrive
    Selectors show if they're selected  
    PMs are colored  
    Coloring text of selector/buttons when new messages arrive  
    Color goes away if mouse is hovered over window

--- Organizer.lua
+++ Organizer.lua
@@ -1,17 +1,22 @@
 script_name('UIF Organizer')
 script_author('Nasif')
 require "lib.moonloader"
-
+local fa = require 'fAwesome5'
 local SE = require 'lib.samp.events'
 local imgui = require 'mimgui'
+
 local new = imgui.new
 local sizeX, sizeY = getScreenResolution()
 local pmArray = {}
 local adminArray = {}
 
+-- Mimgui variables
 local shouldShowMenu = new.bool(true)
 
+-- Variables
 local messageStream = 'pm'
+local haveNewPMs = false
+local haveNewAdminMessages = false
 
 local messagesFrame = imgui.OnFrame(function()
     return shouldShowMenu[0]
@@ -21,15 +26,33 @@ end, function(player)
     imgui.Begin('Organizer', shouldShowMenu)
     player.HideCursor = true
 
+    if imgui.IsAnyItemHovered() then haveNewPMs = false end
+    if imgui.IsAnyItemHovered() then haveNewAdminMessages = false end
+
     -- Selectors
+    -- PM
     imgui.BeginChild("Selectors", imgui.ImVec2(130, 50), true)
-    if imgui.Selectable('Private Messages') then
+    if haveNewPMs then
+        imgui.PushStyleColor(imgui.Col.Text, imgui.ImVec4(1.0, 0.0, 0.0, 1.0))
+    else
+        imgui.PushStyleColor(imgui.Col.Text, imgui.ImVec4(1.0, 1.0, 1.0, 1.0))
+    end
+    if imgui.Selectable('Private Messages', (function () return messageStream == 'pm' end)()) then
         messageStream = 'pm'
     end
+    imgui.PopStyleColor()
 
-    if imgui.Selectable('Admin Messages') then
+    -- Admin
+    if haveNewAdminMessages then
+        imgui.PushStyleColor(imgui.Col.Text, imgui.ImVec4(0.8, 0.8, 0.0, 1.0))
+    else
+        imgui.PushStyleColor(imgui.Col.Text, imgui.ImVec4(0.8, 0.8, 0.8, 0.8))
+    end
+    if imgui.Selectable('Admin Messages', (function () return messageStream ~= 'pm' end)()) then
         messageStream = 'admin'
     end
+    imgui.PopStyleColor()
+
     imgui.EndChild()
 
     imgui.SameLine(0)
@@ -39,9 +62,13 @@ end, function(player)
     if messageStream == 'pm' then
         for i, message in reversedipairs(pmArray) do
             if startsWith(message, "->") then
-                imgui.Text(message)
+                imgui.TextWrapped(message)
             else
-                if imgui.Button(message) then
+                imgui.PushStyleColor(imgui.Col.Text, imgui.ImVec4(1.0, 0.0, 0.0, 1.0));
+                imgui.TextWrapped(message)
+                imgui.PopStyleColor()
+               
+                if imgui.Button('Reply') then
                     sampSetChatInputEnabled(true)
                     playerId = getPlayerIdFromText(message)
                     sampSetChatInputText('/pm ' .. playerId .. ' ')
@@ -81,13 +108,19 @@ function toggleShowMenu()
 end
 
 function SE.onServerMessage(arg1, text)
+    processServerMessage(text)
+end
+
+function processServerMessage(text) 
     if startsWith(text, 'ADMIN:') then
         local nameAction = string.match(text, 'ADMIN: (.*)')
         table.insert(adminArray, nameAction)
+        haveNewAdminMessages = true
     elseif startsWith(text, '>> PM from') then
         local playerName, pmMessage = string.match(text, '>> PM from (.*) %(.*: (.*)')
         local privateMessage = playerName .. ': ' .. pmMessage
         table.insert(pmArray, privateMessage)
+        haveNewPMs = true
     elseif startsWith(text, '>> PM to') then
         local playerName, pmMessage = string.match(text, '>> PM to (.*) .*:(.*)')
         local privateMessage = "->" .. playerName .. ': ' .. pmMessage
@@ -96,7 +129,6 @@ function SE.onServerMessage(arg1, text)
 end
 
 function main()
-
     while not isSampAvailable() do
         wait(0)
     end
@@ -107,7 +139,7 @@ function main()
             toggleShowMenu()
         end
     end
-
+    
     if sampGetCurrentServerAddress() ~= "51.254.85.134" then
         return
     end
